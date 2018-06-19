542830ab864968010ee7c476f6582d6b78dac54b
Egor <4156464@gmail.com>
2018-06-19 21:21:27 +0300

    111

---
+++ src/com/company/ies/Ies.java
@@ -0,0 +1,4 @@
+package com.company.ies;
+
+public class Ies {
+}
---
+++ src/com/company/ies/filos/Filos.java
@@ -0,0 +1,14 @@
+package com.company.ies.filos;
+
+public class Filos {
+    private int a;
+    private String str;
+
+    public int getA() {
+        return a;
+    }
+
+    public String getStr() {
+        return str;
+    }
+}
