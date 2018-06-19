f422df588fbfe277490ee4ff0de21a4971debddd
Egor <4156464@gmail.com>
2018-06-19 21:33:18 +0300

    222

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,16 @@
 package com.company;
 
+import com.company.ies.filos.Filos;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Filos filos = new Filos();
+            //filos.getA();
+            //filos.getStr();
+        System.out.print("Name: " + filos.getStr() + " is " + filos.getA());
 
+            filos.getStr();
     }
 }
--- src/com/company/ies/filos/Filos.java
+++ src/com/company/ies/filos/Filos.java
@@ -1,14 +1,18 @@
 package com.company.ies.filos;
 
 public class Filos {
-    private int a;
-    private String str;
+    private int a = 77;
+    private String str = "Egor";
 
     public int getA() {
         return a;
+
     }
 
     public String getStr() {
         return str;
+
     }
+
+
 }
